349d324b11fad8799293342ac35231b05c503c79
Bostjan Skufca <bostjan@a2o.si>
2012-03-26 01:23:42 +0200

    Added awstats installation

---
+++ modules/a2o-essential-linux-awstats/manifests/init.pp
@@ -0,0 +1,91 @@
+###########################################################################
+# a2o Essential Puppet Modules                                            #
+#-------------------------------------------------------------------------#
+# Copyright (c) 2012 Bostjan Skufca                                       #
+#-------------------------------------------------------------------------#
+# This source file is subject to version 2.0 of the Apache License,       #
+# that is bundled with this package in the file LICENSE, and is           #
+# available through the world-wide-web at the following url:              #
+# http://www.apache.org/licenses/LICENSE-2.0                              #
+#-------------------------------------------------------------------------#
+# Authors: Bostjan Skufca <my_name [at] a2o {dot} si>                     #
+###########################################################################
+
+
+
+### Base class
+class   a2o-essential-linux-awstats::base {
+    $thisPuppetModule = 'a2o-essential-linux-awstats'
+}
+
+
+
+### Software package
+class   a2o-essential-linux-awstats::package   inherits   a2o-essential-linux-awstats::base {
+
+    # Software details
+    $packageName            = 'awstats'
+    $packageSoftware        = 'awstats'
+    $packageSoftwareVersion = '6.95'
+    $packageRelease         = '3'
+    $packageEnsure          = "$packageSoftwareVersion-$packageRelease"
+    $packageTag             = "$packageSoftware-$packageEnsure"
+    $installScriptTpl       = "install-$packageSoftware.sh"
+    $installScript          = "install-$packageTag.sh"
+
+    # Where the packages will be compiled
+    $compileDir             = "/var/src/tools"
+
+    # Global destination directory
+    $destDir                = "/usr/local/$packageTag"
+    $destDirSymlink         = "/usr/local/$packageSoftware"
+    $destDirSymlink_dest    = "$packageTag"
+
+
+    # Installation
+    file { "$compileDir/$installScript":
+	content  => template("$thisPuppetModule/$installScriptTpl"),
+        owner    => root,
+        group    => root,
+        mode     => 755,
+	require  => File['/var/src/build_functions.sh'],
+    }
+    package { "$packageName":
+	provider => 'a2o_linux_compiletool',
+        ensure   => "$packageEnsure",
+	source   => "$compileDir/$installScript",
+	require  => [
+	    File["$compileDir/$installScript"],
+	    Package['perl'],
+	],
+    }
+
+
+    # Symlink
+    file { "$destDirSymlink":
+	ensure   => "$destDirSymlink_dest",
+	require  => Package["$packageName"],
+	backup   => false,
+    }
+}
+
+
+
+### Configuration files and directories
+class   a2o-essential-linux-awstats::files   inherits   a2o-essential-linux-awstats::base {
+
+    file { "/etc/awstats":
+	ensure   => directory,
+        owner    => root,
+        group    => root,
+        mode     => 755,
+    }
+}
+
+
+
+### The final all-containing classes
+class a2o-essential-linux-awstats {
+    include 'a2o-essential-linux-awstats::package'
+    include 'a2o-essential-linux-awstats::files'
+}
---
+++ modules/a2o-essential-linux-awstats/templates/install-awstats.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+###########################################################################
+# a2o Essential Puppet Modules                                            #
+#-------------------------------------------------------------------------#
+# Copyright (c) 2012 Bostjan Skufca                                       #
+#-------------------------------------------------------------------------#
+# This source file is subject to version 2.0 of the Apache License,       #
+# that is bundled with this package in the file LICENSE, and is           #
+# available through the world-wide-web at the following url:              #
+# http://www.apache.org/licenses/LICENSE-2.0                              #
+#-------------------------------------------------------------------------#
+# Authors: Bostjan Skufca <my_name [at] a2o {dot} si>                     #
+###########################################################################
+
+
+
+# Compile directory
+export SRCROOT="<%= compileDir %>" &&
+mkdir -p $SRCROOT &&
+cd $SRCROOT &&
+
+
+
+### Set versions and directories
+export PVERSION_AWSTATS="<%= packageSoftwareVersion %>" &&
+export PDESTDIR_AWSTATS="<%= destDir %>" &&
+
+
+
+### AWstats
+# CheckURI: http://awstats.sourceforge.net/#DOWNLOAD
+# URI: http://awstats.sourceforge.net/
+# Requires: perl
+cd $SRCROOT && . ../build_functions.sh &&
+export PNAME="awstats" &&
+export PVERSION="$PVERSION_AWSTATS" &&
+export PDIR="$PNAME-$PVERSION" &&
+export PFILE="$PDIR.tar.gz" &&
+export PURI="http://mesh.dl.sourceforge.net/project/awstats/AWStats/$PVERSION/$PFILE" &&
+
+rm -rf $PDIR &&
+GetUnpack &&
+
+rm -rf $PDESTDIR_AWSTATS &&
+cp -R $PDIR $PDESTDIR_AWSTATS &&
+
+# Move CGIs to separate dir to allow common system .htaccess and custom .htaccess for client accesses
+cd $PDESTDIR_AWSTATS/wwwroot &&
+mkdir cgis &&
+mv cgi-bin/ cgis/ &&
+ln -s cgis/cgi-bin cgi-bin &&
+
+cd $SRCROOT &&
+rm -rf $PDIR &&
+
+
+
+exit 0
