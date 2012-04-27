99c7f7955375bef81f0365191c98ad4b2b71ee82
Bostjan Skufca <bostjan@a2o.si>
2012-04-27 18:56:45 +0200

    Added generic a2o-linux service definition for a2o-linux-rctool provider

---
+++ modules/a2o-essential-unix/manifests/rctool/service/generic.pp
@@ -0,0 +1,82 @@
+###########################################################################
+# a2o Essential Puppet Modules                                            #
+#-------------------------------------------------------------------------#
+# Copyright (c) 2012 Bostjan Skufca                                       #
+#-------------------------------------------------------------------------#
+# This source file is subject to version 2.0 of the Apache License,       #
+# that is bundled with this package in the file LICENSE, and is           #
+# available through the world-wide-web at the following url:              #
+# http://www.apache.org/licenses/LICENSE-2.0                              #
+#-------------------------------------------------------------------------#
+# Authors: Bostjan Skufca <my_name [at] a2o {dot} si>                     #
+###########################################################################
+
+
+
+### Generic service definition
+#
+# This is used as a template for service definitions in other modules
+# Pass it the service name, require and subscribe definitions and it will generate
+# service resource for you.
+
+define   a2o-essential-unix::rctool::service::generic   ($ensure='running', $require=[], $subscribe=[]) {
+
+
+    ###
+    ### Configuration
+    ###
+    $serviceName   = "$name"
+    $serviceEnsure = "$ensure"
+
+
+    ###
+    ### Startup file definition
+    ###
+    file { "/etc/rc.d/rc.$serviceName":
+        content  => template("$thisPuppetModule/rc.$serviceName"),
+        owner    => root,
+        group    => root,
+
+# Mode property is commented out because service provider a2o_linux_rctool
+# takes care of it through enable property
+#        mode     => $serviceEnsure ? {
+#	    running => 755,
+#	    stopped => 644,
+#	    default => 644,
+#	},
+
+	require     => [
+    	    File['/etc/rc.d/rc._functions'],
+            $require,
+	],
+    }
+
+
+    ###
+    ### Service definition
+    ###
+    service { "a2o-linux-$serviceName":
+        enable      => $serviceEnsure ? {
+	    running => true,
+	    stopped => false,
+	    disbled => false,
+	    default => false,
+	},
+        ensure      => $serviceEnsure ? {
+	    running => running,
+	    stopped => stopped,
+	    disbled => stopped,
+	    default => stopped,
+	},
+	hasrestart  => true,
+        hasstatus   => true,
+	provider    => 'a2o_linux_rctool',
+        require     => [
+	    $require,
+	],
+        subscribe   => [
+    	    File["/etc/rc.d/rc.$serviceName"],
+	    $subscribe,
+	]
+    }
+}
