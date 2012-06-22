2f572754d282f8f2a19908b71fd391bbb23b3963
Bostjan Skufca <bostjan@a2o.si>
2012-06-22 13:21:50 +0200

    Added licensing info to where it was missing and feasible

--- modules/a2o-essential-linux-ganglia/files/python_modules/a2o_raid.py
+++ modules/a2o-essential-linux-ganglia/files/python_modules/a2o_raid.py
@@ -1,3 +1,15 @@
+###########################################################################
+# a2o Essential Puppet Modules                                            #
+#-------------------------------------------------------------------------#
+# Copyright (c) 2012 Bostjan Skufca                                       #
+#-------------------------------------------------------------------------#
+# This source file is subject to version 2.0 of the Apache License,       #
+# that is bundled with this package in the file LICENSE, and is           #
+# available through the world-wide-web at the following url:              #
+# http://www.apache.org/licenses/LICENSE-2.0                              #
+#-------------------------------------------------------------------------#
+# Authors: Bostjan Skufca <my_name [at] a2o {dot} si>                     #
+###########################################################################
 ###
 ###
 ### Import libraries
