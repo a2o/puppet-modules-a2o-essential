4f3bca0add902eb8f8a897ebf19e39e2452550c5
Bostjan Skufca <bostjan@a2o.si>
2012-07-05 00:49:17 +0200

    Added activemq for a2o distro

---
+++ modules/a2o_essential_linux_activemq/manifests/distro/a2o.pp
@@ -0,0 +1,42 @@
+###########################################################################
+# a2o Essential Puppet Modules                                            #
+#-------------------------------------------------------------------------#
+# Copyright (c) 2012 Bostjan Skufca                                       #
+#-------------------------------------------------------------------------#
+# This source file is subject to version 2.0 of the Apache License,       #
+# that is bundled with this package in the file LICENSE, and is           #
+# available through the world-wide-web at the following url:              #
+# http://www.apache.org/licenses/LICENSE-2.0                              #
+#-------------------------------------------------------------------------#
+# Authors: Bostjan Skufca <my_name [at] a2o {dot} si>                     #
+###########################################################################
+
+
+
+### Service: activemq
+class   a2o_essential_linux_activemq::distro::a2o::service   inherits   a2o_essential_linux_activemq::base {
+
+    $require   = [
+        File['/etc/activemq'],
+        File['/opt/activemq'],
+        File['/var/activemq'],
+    ]
+    $subscribe = [
+	Package['activemq'],
+        File['/usr/local/activemq'],
+    ]
+
+    a2o-essential-unix::rctool::service::generic { 'activemq':
+        require   => $require,
+        subscribe => $subscribe,
+    }
+}
+
+
+
+### The final all-containing classes
+class a2o_essential_linux_activemq::distro::a2o {
+    include 'a2o_essential_linux_activemq::package'
+    include 'a2o_essential_linux_activemq::files'
+    include 'a2o_essential_linux_activemq::distro::a2o::service'
+}
