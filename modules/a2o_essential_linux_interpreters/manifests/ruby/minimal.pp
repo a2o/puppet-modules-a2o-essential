b35b25bec2c6767dbce56c6acbe73f4fd5dacf0a
Bostjan Skufca <bostjan@a2o.si>
2012-07-18 16:06:40 +0200

    Added also Minimal variation of redmine installation

---
+++ modules/a2o_essential_linux_interpreters/manifests/ruby/minimal.pp
@@ -0,0 +1,20 @@
+###########################################################################
+# a2o Essential Puppet Modules                                            #
+#-------------------------------------------------------------------------#
+# Copyright (c) 2012 Bostjan Skufca                                       #
+#-------------------------------------------------------------------------#
+# This source file is subject to version 2.0 of the Apache License,       #
+# that is bundled with this package in the file LICENSE, and is           #
+# available through the world-wide-web at the following url:              #
+# http://www.apache.org/licenses/LICENSE-2.0                              #
+#-------------------------------------------------------------------------#
+# Authors: Bostjan Skufca <my_name [at] a2o {dot} si>                     #
+###########################################################################
+
+
+
+class a2o_essential_linux_interpreters::ruby::minimal {
+
+    include 'a2o_essential_linux_interpreters::ruby::package'
+    include 'a2o_essential_linux_interpreters::ruby::symlinks'
+}
