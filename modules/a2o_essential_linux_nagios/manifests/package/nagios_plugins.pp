e54ca5707ee44b2ae1e870372e3c119328b9fe3e
Bostjan Skufca <bostjan@a2o.si>
2012-08-15 22:35:48 +0200

    Nagios plugins: added forward and reverse DNS checks for hostnames

--- modules/a2o_essential_linux_nagios/manifests/package/nagios_plugins.pp
+++ modules/a2o_essential_linux_nagios/manifests/package/nagios_plugins.pp
@@ -34,12 +34,25 @@ class   a2o_essential_linux_nagios::package::nagios_plugins   inherits   a2o_ess
     a2o-essential-unix::compiletool::package::generic { "$packageTag": require => $require, }
 
 
+    ### Additional check scripts
+    File {
+	owner   => root,
+	group   => root,
+	mode    => 755,
+	require => Package["$softwareName"],
+	before  => File["$destDirSymlink"],
+    }
+    file { "$destDir/libexec/check_dns_host":           source  => "puppet:///modules/$thisPuppetModule/check_dns_host"         }
+    file { "$destDir/libexec/check_dns_host_reverse":   source  => "puppet:///modules/$thisPuppetModule/check_dns_host_reverse" }
+
+
     ### Symlink
     file { "$destDirSymlink":
         ensure  => "$destDirSymlink_dest",
 	require => [
 	    Package["$softwareName"],
 	],
+	before   => undef,
 	backup   => false,
     }
 }
