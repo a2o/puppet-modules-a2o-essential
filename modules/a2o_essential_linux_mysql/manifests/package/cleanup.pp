c5eea61f730e0c9b50eecb698a2f3baeb8b26c41
Bostjan Skufca <bostjan@a2o.si>
2012-08-30 17:25:17 +0200

    Removed mysql cleanup dependency - mysql service

--- modules/a2o_essential_linux_mysql/manifests/package/cleanup.pp
+++ modules/a2o_essential_linux_mysql/manifests/package/cleanup.pp
@@ -19,7 +19,6 @@ class   a2o_essential_linux_mysql::package::cleanup   inherits   a2o_essential_l
     $require = [
 	Package['mysql'],
 	File['/usr/local/mysql'],
-	Service['a2o-linux-mysqld'],
     ]
 
     a2o-essential-unix::compiletool::package::remove { 'mysql-5.1.40-1': compileDir => $compileDir, require => $require, }
