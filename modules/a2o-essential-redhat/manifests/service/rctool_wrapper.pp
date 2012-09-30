5a14e308e0b44ef803d06e257dafbe851dba3ea7
Bostjan Skufca <bostjan@a2o.si>
2012-09-30 22:03:45 +0200

    RedHat: Added rctool wrapper and generic service definitions

---
+++ modules/a2o-essential-redhat/manifests/service/rctool_wrapper.pp
@@ -0,0 +1,90 @@
+###########################################################################
+# a2o Essential Puppet Modules                                            #
+#-------------------------------------------------------------------------#
+# Copyright (c) 2012 Bostjan Skufca                                       #
+#-------------------------------------------------------------------------#
+# This source file is subject to version 2.0 of the Apache License,       #
+# that is bundled with this package in the file LICENSE, and is           #
+# available through the world-wide-web at the following url:              #
+# http://www.apache.org/licenses/LICENSE-2.0                              #
+#-------------------------------------------------------------------------#
+# Authors: Bostjan Skufca <my_name [at] a2o {dot} si>                     #
+###########################################################################
+
+
+
+### Generic service definition: RedHat
+#
+# This is used as a template for service definitions in other modules
+# Pass it the service name, require and subscribe definitions and it will generate
+# service resource for you.
+
+define   a2o-essential-redhat::service::rctool_wrapper   ($ensure='running', $require=[], $subscribe=[]) {
+
+
+    ###
+    ### Configuration
+    ###
+    $serviceName   = "$name"
+    $serviceEnsure = "$ensure"
+
+
+
+    ###
+    ### Startup files
+    ###
+    file { "/etc/rc.d/rc.$serviceName":
+        content  => template("$thisPuppetModule/rc.$serviceName"),
+        owner    => root,
+        group    => root,
+        mode     => 755,
+	require     => [
+	    File['/etc/rc.tool/common'],
+	    File['/etc/rc.d/rc._functions'], ### FIXME remove, compatibility
+            $require,
+	],
+    }
+    file { "/etc/init.d/$serviceName":
+        content  => template("$thisPuppetModule/redhat/$serviceName"),
+        owner    => root,
+        group    => root,
+        mode     => 755,
+	require     => [
+            $require,
+	    $subscribe,
+	    File["/etc/rc.d/rc.$serviceName"],
+	    File['/etc/rc.tool/wrapper.redhat'],
+        ],
+    }
+
+
+
+    ###
+    ### Service definition
+    ###
+    service { "$serviceName":
+        enable      => $serviceEnsure ? {
+	    running => true,
+	    stopped => false,
+	    disbled => false,
+	    default => false,
+	},
+        ensure      => $serviceEnsure ? {
+	    running => running,
+	    stopped => stopped,
+	    disbled => stopped,
+	    default => stopped,
+	},
+	hasrestart  => true,
+        hasstatus   => true,
+	provider    => 'redhat',
+        require     => [
+	    $require,
+	],
+        subscribe   => [
+	    File["/etc/init.d/$serviceName"],
+	    File["/etc/rc.d/rc.$serviceName"],
+	    $subscribe,
+	]
+    }
+}
