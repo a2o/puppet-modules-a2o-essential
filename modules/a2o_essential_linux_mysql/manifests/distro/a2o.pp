f75e69191a53335dca80f6713938abb8f8bd601f
Bostjan Skufca <bostjan@a2o.si>
2012-10-04 23:42:11 +0200

    Added mysql a2o definition, ported from a2o repo

---
+++ modules/a2o_essential_linux_mysql/manifests/distro/a2o.pp
@@ -0,0 +1,39 @@
+###########################################################################
+# a2o Essential Puppet Modules                                            #
+#-------------------------------------------------------------------------#
+# Copyright (c) 2012 Bostjan Skufca                                       #
+#-------------------------------------------------------------------------#
+# This source file is subject to version 2.0 of the Apache License,       #
+# that is bundled with this package in the file LICENSE, and is           #
+# available through the world-wide-web at the following url:              #
+# http://www.apache.org/licenses/LICENSE-2.0                              #
+#-------------------------------------------------------------------------#
+# Authors: Bostjan Skufca <my_name [at] a2o {dot} si>                     #
+###########################################################################
+
+
+
+### Service: mysql
+class   a2o_essential_linux_mysql::distro::a2o::service   inherits   a2o_essential_linux_mysql::distro::base {
+
+    $requireA2o = [
+	$require,
+	File['/etc/ssl/certs/mysql.key'],
+	File['/etc/ssl/certs/mysql.cert'],
+    ]
+
+    a2o-essential-unix::rctool::service::generic { 'mysqld':
+        require   => $requireA2o,
+        subscribe => $subscribe,
+    }
+}
+
+
+
+### The final all-containing classes
+class   a2o_essential_linux_mysql::distro::a2o {
+
+    include 'a2o_essential_linux_mysql::distro::common'
+    include 'a2o_essential_linux_mysql::distro::a2o::service'
+    include 'a2o_essential_linux_mysql::files::symlinks_lib'
+}
---
+++ modules/a2o_essential_linux_mysql/manifests/files/symlinks_lib.pp
@@ -0,0 +1,32 @@
+###########################################################################
+# a2o Essential Puppet Modules                                            #
+#-------------------------------------------------------------------------#
+# Copyright (c) 2012 Bostjan Skufca                                       #
+#-------------------------------------------------------------------------#
+# This source file is subject to version 2.0 of the Apache License,       #
+# that is bundled with this package in the file LICENSE, and is           #
+# available through the world-wide-web at the following url:              #
+# http://www.apache.org/licenses/LICENSE-2.0                              #
+#-------------------------------------------------------------------------#
+# Authors: Bostjan Skufca <my_name [at] a2o {dot} si>                     #
+###########################################################################
+
+
+
+### Symlinks to programs and directories
+class   a2o_essential_linux_mysql::files::symlinks_lib   inherits   a2o_essential_linux_mysql::base {
+
+    # Template
+    File {
+        owner    => root,
+        group    => root,
+        mode     => 755,
+	require  => File['/usr/local/mysql'],
+    }
+
+    # Library symlink - FIXME why? Postfix again?
+    file { '/usr/lib/libmysqlclient.so'     :   ensure => '/usr/local/mysql/lib/mysql/libmysqlclient.so',      backup => false, force => true, }
+    file { '/usr/lib/libmysqlclient.so.16'  :   ensure => '/usr/local/mysql/lib/mysql/libmysqlclient.so.16',   backup => false, force => true, }
+    file { '/usr/lib/libmysqlclient_r.so'   :   ensure => '/usr/local/mysql/lib/mysql/libmysqlclient_r.so',    backup => false, force => true, }
+    file { '/usr/lib/libmysqlclient_r.so.16':   ensure => '/usr/local/mysql/lib/mysql/libmysqlclient_r.so.16', backup => false, force => true, }
+}
