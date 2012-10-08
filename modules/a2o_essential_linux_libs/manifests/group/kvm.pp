595742f91a319f5a8a696451c026210260d0ebac
Bostjan Skufca <bostjan@a2o.si>
2012-10-08 03:01:41 +0200

    Added libs group called ::kvm, with kvm dependencies

---
+++ modules/a2o_essential_linux_libs/manifests/group/kvm.pp
@@ -0,0 +1,26 @@
+###########################################################################
+# a2o Essential Puppet Modules                                            #
+#-------------------------------------------------------------------------#
+# Copyright (c) 2012 Bostjan Skufca                                       #
+#-------------------------------------------------------------------------#
+# This source file is subject to version 2.0 of the Apache License,       #
+# that is bundled with this package in the file LICENSE, and is           #
+# available through the world-wide-web at the following url:              #
+# http://www.apache.org/licenses/LICENSE-2.0                              #
+#-------------------------------------------------------------------------#
+# Authors: Bostjan Skufca <my_name [at] a2o {dot} si>                     #
+###########################################################################
+
+
+
+class a2o_essential_linux_libs::group::kvm {
+    include 'a2o-essential-linux-libs::curl'
+    include 'a2o-essential-linux-libs::cyrus-sasl'
+    include 'a2o-essential-linux-libs::glib'
+    include 'a2o-essential-linux-libs::gnutls'
+    include 'a2o_essential_linux_libs::libcap_ng'
+    include 'a2o-essential-linux-libs::libgcrypt'
+    include 'a2o-essential-linux-libs::libiconv'
+    include 'a2o_essential_linux_libs::libnl'
+    include 'a2o_essential_linux_libs::zlib'
+}
