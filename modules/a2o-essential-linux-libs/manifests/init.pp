6ba6b51b01d08bcbc6a4652b29f24fc88a568323
Bostjan Skufca <bostjan@a2o.si>
2012-10-12 20:43:27 +0200

    Added initial nrpe dependency class in libs

--- modules/a2o-essential-linux-libs/manifests/init.pp
+++ modules/a2o-essential-linux-libs/manifests/init.pp
@@ -88,6 +88,13 @@ class a2o-essential-linux-libs::nagios {
     include 'a2o-essential-linux-libs::libjpeg'
     include 'a2o-essential-linux-libs::libpng'
 }
+class a2o-essential-linux-libs::nrpe {
+    include 'a2o_essential_linux_interpreter::perl::minimal'
+    include 'a2o-essential-linux-libs::berkeley-db'
+    include 'a2o-essential-linux-libs::cyrus-sasl'
+    include 'a2o-essential-linux-openldap::package'
+    include 'a2o_essential_linux_mysql::package'
+}
 class a2o-essential-linux-libs::ruby {
     include 'a2o-essential-linux-libs::yaml'
 }
