a3cd7fdcfd44ed765ea571a90fac4ad27a3530e5
Bostjan Skufca <bostjan@a2o.si>
2012-12-31 22:22:43 +0100

    Updated NRPE with check for gmond metrics, will be used for ganglia monitoring now; refactoring, added RAID check,

--- modules/a2o_essential_linux_nagios/manifests/files/nrpe.pp
+++ modules/a2o_essential_linux_nagios/manifests/files/nrpe.pp
@@ -13,19 +13,58 @@
 
 
 
-### NRPE directories and files
-class   a2o_essential_linux_nagios::files::nrpe   inherits   a2o_essential_linux_nagios::base {
+### Base class
+class   a2o_essential_linux_nagios::files::nrpe::base   inherits   a2o_essential_linux_nagios::base {
 
     File {
 	owner => root,
 	group => root,
     }
+}
 
-    file { '/etc/nrpe':                ensure => directory, mode => 750 }
-    file { '/etc/nrpe/conf.d':         ensure => directory, mode => 750 }
-    file { '/etc/nrpe/nrpe.cfg':       ensure => present,   content => template("$thisPuppetModule/nrpe.cfg"),     mode => 640 }
-    file { '/etc/nrpe/commands.cfg':   ensure => present,   content => template("$thisPuppetModule/commands.cfg"), mode => 640 }
+
+
+### Directories
+class   a2o_essential_linux_nagios::files::nrpe::dirs   inherits   a2o_essential_linux_nagios::files::nrpe::base {
+
+    file { '/etc/nrpe':                ensure => directory, mode => 755 }
+    file { '/etc/nrpe/conf.d':         ensure => directory, mode => 755 }
+    file { '/etc/nrpe/plugins':        ensure => directory, mode => 755 }
 
     # Compatibility
-    file { '/etc/nagios/nrpe':     ensure => '/etc/nrpe' }
+    file { '/etc/nagios/nrpe':         ensure => link, target => '/etc/nrpe' }
+}
+
+
+
+### Main config files
+class   a2o_essential_linux_nagios::files::nrpe::config   inherits   a2o_essential_linux_nagios::files::nrpe::base {
+
+    file { '/etc/nrpe/nrpe.cfg':       ensure => present,   content => template("$thisPuppetModule/nrpe.cfg"),     mode => 640 }
+    file { '/etc/nrpe/commands.cfg':   ensure => present,   content => template("$thisPuppetModule/commands.cfg"), mode => 640 }
+}
+
+
+
+### Custom plugins
+class   a2o_essential_linux_nagios::files::nrpe::plugins   inherits   a2o_essential_linux_nagios::files::nrpe::base {
+
+    File {
+	require => Package['nagios-plugins'],
+	before  => File['/usr/local/nagios-plugins'],
+	mode    => 755,
+    }
+
+    file { '/etc/nrpe/plugins/check_dns_host':           source  => "puppet:///modules/$thisPuppetModule/check_dns_host"         }
+    file { '/etc/nrpe/plugins/check_dns_host_reverse':   source  => "puppet:///modules/$thisPuppetModule/check_dns_host_reverse" }
+    file { '/etc/nrpe/plugins/check_gmond':              source  => "puppet:///modules/$thisPuppetModule/check_gmond"            }
+}
+
+
+
+### All together now
+class   a2o_essential_linux_nagios::files::nrpe {
+    include 'a2o_essential_linux_nagios::files::nrpe::dirs'
+    include 'a2o_essential_linux_nagios::files::nrpe::config'
+    include 'a2o_essential_linux_nagios::files::nrpe::plugins'
 }
