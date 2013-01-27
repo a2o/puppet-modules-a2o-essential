f690573d693a5a389687ab06666ed80cf2c1b58f
Bostjan Skufca <bostjan@a2o.si>
2013-01-27 21:23:42 +0100

    Added group of libraries for nagios

---
+++ modules/a2o_essential_linux_libs/manifests/group/nagios.pp
@@ -0,0 +1,18 @@
+###########################################################################
+# a2o Essential Puppet Modules                                            #
+#-------------------------------------------------------------------------#
+# Copyright (c) Bostjan Skufca                                            #
+#-------------------------------------------------------------------------#
+# This source file is subject to version 2.0 of the Apache License,       #
+# that is bundled with this package in the file LICENSE, and is           #
+# available through the world-wide-web at the following url:              #
+# http://www.apache.org/licenses/LICENSE-2.0                              #
+#-------------------------------------------------------------------------#
+# Authors: Bostjan Skufca <my_name [at] a2o {dot} si>                     #
+###########################################################################
+
+
+
+class a2o_essential_linux_libs::group::nagios {
+    include 'a2o-essential-linux-libs::ganglia'
+}
