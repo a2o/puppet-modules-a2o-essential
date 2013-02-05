d36ff6c72fe72320c5dc4e804ed2b638b7b001b2
Bostjan Skufca <bostjan@a2o.si>
2013-02-05 03:24:36 +0100

    Added ubuntu openssh definition, default stuff, with only dns enabled and root login to without-password

---
+++ modules/a2o_essential_ubuntu_openssh/manifests/packages.pp
@@ -0,0 +1,27 @@
+###########################################################################
+# a2o Essential Puppet Modules                                            #
+#-------------------------------------------------------------------------#
+# Copyright (c) Bostjan Skufca                                            #
+#-------------------------------------------------------------------------#
+# This source file is subject to version 2.0 of the Apache License,       #
+# that is bundled with this package in the file LICENSE, and is           #
+# available through the world-wide-web at the following url:              #
+# http://www.apache.org/licenses/LICENSE-2.0                              #
+#-------------------------------------------------------------------------#
+# Authors: Bostjan Skufca <my_name [at] a2o {dot} si>                     #
+###########################################################################
+
+
+
+### Packages
+class   a2o_essential_ubuntu_openssh::packages   inherits   a2o_essential_ubuntu_openssh::base {
+
+    Package {
+	provider => apt,
+	ensure   => present,
+    }
+
+    ### All the packages that are required
+    package { 'openssh-client':   }
+    package { 'openssh-server':   }
+}
---
+++ modules/a2o_essential_ubuntu_openssh/manifests/service.pp
@@ -0,0 +1,27 @@
+###########################################################################
+# a2o Essential Puppet Modules                                            #
+#-------------------------------------------------------------------------#
+# Copyright (c) Bostjan Skufca                                            #
+#-------------------------------------------------------------------------#
+# This source file is subject to version 2.0 of the Apache License,       #
+# that is bundled with this package in the file LICENSE, and is           #
+# available through the world-wide-web at the following url:              #
+# http://www.apache.org/licenses/LICENSE-2.0                              #
+#-------------------------------------------------------------------------#
+# Authors: Bostjan Skufca <my_name [at] a2o {dot} si>                     #
+###########################################################################
+
+
+
+### Service: ssh
+class   a2o_essential_ubuntu_openssh::service   inherits   a2o_essential_ubuntu_openssh::base {
+
+    a2o-essential-debian::service::generic { 'ssh':
+	require   => [
+	    Package['openssh-server'],
+	],
+	subscribe => [
+	    File['/etc/ssh/sshd_config'],
+	],
+    }
+}
