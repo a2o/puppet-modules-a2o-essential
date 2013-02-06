eb0f228b50aa9234990c28e57d037e8a63eebb4e
Bostjan Skufca <bostjan@a2o.si>
2013-02-06 04:34:29 +0100

    NRPE: added RAID checks for tool ipssend, fixed arcconf, added nrpe raid_check tool, fixed command in nrpe config

--- modules/a2o_essential_linux_nagios/manifests/files/nrpe.pp
+++ modules/a2o_essential_linux_nagios/manifests/files/nrpe.pp
@@ -58,6 +58,7 @@ class   a2o_essential_linux_nagios::files::nrpe::plugins   inherits   a2o_essent
     file { '/etc/nrpe/plugins/check_dns_host':           source  => "puppet:///modules/$thisPuppetModule/check_dns_host"         }
     file { '/etc/nrpe/plugins/check_dns_host_reverse':   source  => "puppet:///modules/$thisPuppetModule/check_dns_host_reverse" }
     file { '/etc/nrpe/plugins/check_gmond':              source  => "puppet:///modules/$thisPuppetModule/check_gmond"            }
+    file { '/etc/nrpe/plugins/check_raid':               source  => "puppet:///modules/$thisPuppetModule/check_raid" }
 
     # Raid check scripts
     file { '/opt/scripts/raid':
