0681d8eef37fbe1d667e0a6d9523931909b514c1
Bostjan Skufca <bostjan@a2o.si>
2013-03-08 17:00:05 +0100

    Bind: added suse daemon startup definition and script

---
+++ modules/a2o_essential_linux_bind/manifests/distro/suse.pp
@@ -0,0 +1,31 @@
+###########################################################################
+# a2o Essential Puppet Modules                                            #
+#-------------------------------------------------------------------------#
+# Copyright (c) Bostjan Skufca                                            #
+#-------------------------------------------------------------------------#
+# This source file is subject to version 2.0 of the Apache License,       #
+# that is bundled with this package in the file LICENSE, and is           #
+# available through the world-wide-web at the following url:              #
+# http://www.apache.org/licenses/LICENSE-2.0                              #
+#-------------------------------------------------------------------------#
+# Authors: Bostjan Skufca <my_name [at] a2o {dot} si>                     #
+###########################################################################
+
+
+
+### Service: named
+class   a2o_essential_linux_bind::distro::suse::service   inherits   a2o_essential_linux_bind::distro::service_base {
+
+    a2o-essential-suse::service::rctool_wrapper { 'named':
+        require   => $require,
+        subscribe => $subscribe,
+    }
+}
+
+
+
+### Final all-containing class
+class   a2o_essential_linux_bind::distro::suse {
+    include 'a2o_essential_linux_bind::distro::common'
+    include 'a2o_essential_linux_bind::distro::suse::service'
+}
