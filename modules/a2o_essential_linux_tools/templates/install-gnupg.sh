18f301a891cda2a533b755ac2f0d5e6cee9cc0e5
Bostjan Skufca <bostjan@a2o.si>
2013-03-17 01:05:46 +0100

    Added gnupg to all systems, included it in standard toolset

---
+++ modules/a2o_essential_linux_tools/templates/install-gnupg.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+###########################################################################
+# a2o Essential Puppet Modules                                            #
+#-------------------------------------------------------------------------#
+# Copyright (c) Bostjan Skufca                                            #
+#-------------------------------------------------------------------------#
+# This source file is subject to version 2.0 of the Apache License,       #
+# that is bundled with this package in the file LICENSE, and is           #
+# available through the world-wide-web at the following url:              #
+# http://www.apache.org/licenses/LICENSE-2.0                              #
+#-------------------------------------------------------------------------#
+# Authors: Bostjan Skufca <my_name [at] a2o {dot} si>                     #
+###########################################################################
+
+
+
+### Init
+export SRCROOT="<%= compileDir %>" &&
+mkdir -p $SRCROOT &&
+cd $SRCROOT &&
+
+
+
+### Set versions and directories
+export PVERSION_SW="<%= softwareVersion %>" &&
+
+
+
+### Install
+cd $SRCROOT && . ../build_functions.sh &&
+export PNAME="gnupg" &&
+export PVERSION="$PVERSION_SW" &&
+export PDIR="$PNAME-$PVERSION" &&
+export PFILE="$PDIR.tar.bz2" &&
+export PURI="ftp://ftp.gnupg.org/gcrypt/gnupg/$PFILE" &&
+
+rm -rf $PDIR &&
+GetUnpackCd &&
+
+./configure &&
+make &&
+make install &&
+
+cd $SRCROOT &&
+rm -rf $PDIR &&
+
+
+
+true
