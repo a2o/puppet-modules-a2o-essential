94f5b21318155f2b76bfe840b2ad7b02e770194b
Bostjan Skufca <bostjan@a2o.si>
2013-04-08 18:40:50 +0200

    Fixed symlinks for mysql

--- modules/a2o_essential_linux_mysql/manifests/files/symlinks_lib.pp
+++ modules/a2o_essential_linux_mysql/manifests/files/symlinks_lib.pp
@@ -24,14 +24,20 @@ class   a2o_essential_linux_mysql::files::symlinks_lib   inherits   a2o_essentia
         require  => File['/usr/local/mysql'],
     }
 
-    # Library symlink - FIXME why? Postfix again?
-    file { '/usr/lib/libmysqlclient.so'     :   ensure => link, target => '/usr/local/mysql/lib/mysql/libmysqlclient.so',          backup => false }
-    file { '/usr/lib/libmysqlclient_r.so'   :   ensure => link, target => '/usr/local/mysql/lib/mysql/libmysqlclient_r.so',        backup => false }
+    # Library symlink - FIXME why? Postfix again? DOVECOT
     file { '/usr/lib/libmysqlclient.so.16'  :   ensure => link, target => '/usr/local/mysql-5.1/lib/mysql/libmysqlclient.so.16',   backup => false }
     file { '/usr/lib/libmysqlclient_r.so.16':   ensure => link, target => '/usr/local/mysql-5.1/lib/mysql/libmysqlclient_r.so.16', backup => false }
 
     if $version_major == '5.5' {
-#	file { '/usr/lib/libmysqlclient.so.18'  :   ensure => link, target => '/usr/local/mysql-5.5/lib/mysql/libmysqlclient.so.16',   backup => false }
-#	file { '/usr/lib/libmysqlclient_r.so.18':   ensure => link, target => '/usr/local/mysql-5.5/lib/mysql/libmysqlclient_r.so.16', backup => false }
+	file { '/usr/lib/libmysqlclient.so.18'  :   ensure => link, target => '/usr/local/mysql-5.5/lib/libmysqlclient.so.18',   backup => false }
+	file { '/usr/lib/libmysqlclient_r.so.18':   ensure => link, target => '/usr/local/mysql-5.5/lib/libmysqlclient_r.so.18', backup => false }
+    }
+
+    if $version_major == '5.5' {
+	file { '/usr/lib/libmysqlclient.so'     :   ensure => link, target => 'libmysqlclient.so.18',   backup => false }
+	file { '/usr/lib/libmysqlclient_r.so'   :   ensure => link, target => 'libmysqlclient_r.so.18', backup => false }
+    } else {
+	file { '/usr/lib/libmysqlclient.so'     :   ensure => link, target => 'libmysqlclient.so.16',   backup => false }
+	file { '/usr/lib/libmysqlclient_r.so'   :   ensure => link, target => 'libmysqlclient_r.so.16', backup => false }
     }
 }
